ede92175d58748a03e34fb20b53deb2ea0ae511f
c-i-a-s-t-e-k <jezowdominik@duck.com>
2024-04-17 01:07:42 +0200

    clear map

--- project/main.cpp
+++ project/main.cpp
@@ -8,7 +8,8 @@ enum Orientation {
     HORIZONTAL
 };
 
-int **create_map(char **map_str, int height, int width, std::pair<int,int> &red_car_position, Orientation &orientation) {
+int **
+create_map(char **map_str, int height, int width, std::pair<int, int> &red_car_position, Orientation &orientation) {
     std::list<std::pair<int, int>> vertical;
     std::list<std::pair<int, int>> horizontal;
     int **map = new int *[height];
@@ -25,7 +26,7 @@ int **create_map(char **map_str, int height, int width, std::pair<int,int> &red_
                     break;
                 case 'o':
                     map[i][j] = -1;
-                    red_car_position = std::pair(i,j);
+                    red_car_position = std::pair(i, j);
                     break;
                 case 'a':
                     vertical.emplace_back(i, j);
@@ -39,9 +40,9 @@ int **create_map(char **map_str, int height, int width, std::pair<int,int> &red_
     }
 
 //    check red car orientation
-    if(map_str[red_car_position.first][red_car_position.second - 1] == 'o')
+    if (map_str[red_car_position.first][red_car_position.second - 1] == 'o')
         orientation = VERTICAL;
-    else if(map_str[red_car_position.first -1][red_car_position.second] == 'o')
+    else if (map_str[red_car_position.first - 1][red_car_position.second] == 'o')
         orientation = HORIZONTAL;
 
 //    fill map with cars
@@ -70,6 +71,19 @@ int **create_map(char **map_str, int height, int width, std::pair<int,int> &red_
     return map;
 }
 
+void clear_map(int **map, int height) {
+    if (map != nullptr) {
+        for (int i = 0; i < height; i++) {
+            if (map[i] != nullptr) {
+                delete[] map[i];
+                map[i] = nullptr;
+            }
+        }
+        delete[] map;
+        map = nullptr;
+    }
+}
+
 int main(int argc, char *argv[]) {
     std::cout << "Hello, World! " << argc << std::endl;
     int width, height, moves;
@@ -77,8 +91,10 @@ int main(int argc, char *argv[]) {
     height = atoi(argv[2]);
     moves = atoi(argv[3]);
     char **map_str = &argv[4];
-    std::pair<int,int> start_pos;
+    std::pair<int, int> start_pos;
     Orientation start_orient;
     int **map = create_map(map_str, height, width, start_pos, start_orient);
 //    std::cout<<start_pos.first<<","<<start_pos.second<<std::endl;
+
+    clear_map(map, height);
 }
